95ed2942c33c06167ce34accb189488c2432109e
Raudel Flores <rawdell@outlook.com>
2020-01-31 18:41:25 -0600

    setup a form input, handleChanges, and a submit function to add new tasks

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,19 +1,35 @@
-import React, { useReducer } from 'react';
-
+import React, { useReducer, useState } from 'react';
 import { initialState, reducer } from '../reducers';
 
 const Todos = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
+    const [newTask, setNewTask] = useState('');
+
+    const handleChanges = e => {
+        setNewTask(e.target.value)
+    }
+
+    const submitTask = e => {
+        e.preventDefault();
+        dispatch({ type: 'ADD_TASK', payload: newTask})
+    }
+
     console.log('what', state);
     return (
-        <div className='todoContainer'>
-        {state.map((value) => {
-            return (
-                <div className='todo' key={value.id}>
-                    <h2>{value.item}</h2>
-                </div>
-            )
-        })}
+        <div className='container'>
+            <form className='addContainer'>
+                <input className='addInput' type='text' name='newTask' value={newTask} onChange={handleChanges}/>
+                <button onClick={submitTask}>Add</button>
+            </form>
+            <div className='todoContainer'>
+                {state.map((value) => {
+                    return (
+                        <div className='todo' key={value.id}>
+                            <h2>{value.item}</h2>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
